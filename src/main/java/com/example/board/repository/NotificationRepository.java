963e54890053d4f450e521745c88e28dd89ac3c7
alswn <minju139@naver.com>
2026-06-29 00:36:36 +0900

    feat: Notification 엔티티 및 Repository 구현

---
+++ src/main/java/com/example/board/repository/NotificationRepository.java
@@ -0,0 +1,13 @@
+package com.example.board.repository;
+
+import com.example.board.domain.Notification;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface NotificationRepository extends JpaRepository<Notification, Long> {
+
+    Page<Notification> findByReceiverIdOrderByCreatedAtDesc(Long receiverId, Pageable pageable);
+
+    long countByReceiverIdAndIsReadFalse(Long receiverId);
+}
