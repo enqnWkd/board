e2454489e7d975f401757c34c88b469058889789
alswn <minju139@naver.com>
2026-06-29 00:06:08 +0900

    fix: 부하테스트 중 주석처리했던 로직 복구

--- src/main/java/com/example/board/service/ArticleLikeService.java
+++ src/main/java/com/example/board/service/ArticleLikeService.java
@@ -95,7 +95,6 @@ public class ArticleLikeService {
         return result;
     }
 
-    /*
     // 특정 사용자가 어떤 게시글에 좋아요했는지 한 번에 조회
     public Map<Long, Boolean> getUserLikesForArticles(List<Long> articleIds, Long userId) {
         Map<Long, Boolean> result = new HashMap<>();
@@ -111,5 +110,4 @@ public class ArticleLikeService {
         return result;
     }
 
-     */
 }
