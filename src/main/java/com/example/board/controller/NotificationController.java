57bd0c039a99a25e146194e93ea7c634fdd79daa
alswn <minju139@naver.com>
2026-06-29 00:10:48 +0900

    feat: WebSocket/STOMP 기본 설정

---
+++ src/main/java/com/example/board/controller/NotificationController.java
@@ -0,0 +1,19 @@
+package com.example.board.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.util.HtmlUtils;
+
+@Slf4j
+@Controller
+public class NotificationController {
+
+    @MessageMapping("/sendMessage")
+    @SendTo("/topic/notifications")
+    public String sendMessage(String message) {
+        log.info("Received message : {}", message);
+        return message;
+    }
+}
--- src/main/java/com/example/board/service/CommentService.java
+++ src/main/java/com/example/board/service/CommentService.java
@@ -12,12 +12,14 @@ import com.example.board.repository.ArticleRepository;
 import com.example.board.repository.CommentRepository;
 import com.example.board.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.stream.Collectors;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class CommentService {
